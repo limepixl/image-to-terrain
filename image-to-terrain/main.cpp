41e1c2f61d23b0875928bd8a03a499885f9971f7
Stefan Ivanovski <stefs.ivanovski@gmail.com>
2019-01-27 19:26:45 +0100

    FPS counter and updated code

--- image-to-terrain/main.cpp
+++ image-to-terrain/main.cpp
@@ -7,39 +7,12 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "RM/ResourceManagement.hpp"
 
 const int WIDTH = 1280;
 const int HEIGHT = 720;
 
-const char *vertexSource = 
-"#version 460 core\n"
-"layout(location = 0) in vec3 aPos;\n"
-"layout(location = 1) in vec2 aTexPos;\n"
-"out vec2 texPos;\n"
-"out float y;\n"
-"out vec4 textureSample;\n"
-"uniform mat4 model;\n"
-"uniform mat4 view;\n"
-"uniform mat4 projection;\n"
-"uniform sampler2D tex;\n"
-"void main() {\n"
-"vec4 sampled = texture(tex, aTexPos);\n"
-"float currentY = sampled.r;\n"
-"gl_Position = projection * view * model * vec4(aPos.x, currentY * 10.0, aPos.z, 1.0);\n"
-"y = currentY;\n"
-"textureSample = sampled;\n"
-"}";
-
-const char *fragmentSource = 
-"#version 460 core\n"
-"in float y;\n"
-"in vec4 textureSample;\n"
-"out vec4 color;\n"
-"void main() {\n"
-"color = vec4(1.0, 1.0, 1.0, 1.0) * y;\n"
-"}\n";
+void processInput(GLFWwindow* window, float& scale);
 
 int main() {
 	// GLFW init
@@ -50,7 +23,7 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Window and context creation
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Window", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Window", nullptr, nullptr);
 	if(window == nullptr) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
@@ -58,92 +31,42 @@ int main() {
 	}
 	glfwMakeContextCurrent(window);
 
+	// Delta time calcs
+	double deltaTime = 0.0;
+	double lastFrame = 0.0;
+
 	// GLAD init
 	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 	glViewport(0, 0, WIDTH, HEIGHT);
 	glEnable(GL_DEPTH_TEST);
 
 	// Shaders
-	unsigned int vertex = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex, 1, &vertexSource, 0);
-	glCompileShader(vertex);
+	Shader basicShader = RM::loadShaders("C:/dev/GitHub/image-to-terrain/image-to-terrain/res/shaders/basicV.glsl", 
+									     "C:/dev/GitHub/image-to-terrain/image-to-terrain/res/shaders/basicF.glsl");
 
-	int compiled;
-	glGetShaderiv(vertex, GL_COMPILE_STATUS, &compiled);	// Check compilation status
-	if(compiled != GL_TRUE)
-	{
-		int log_length = 0;
-		char message[512];
-		glGetShaderInfoLog(vertex, 512, &log_length, message);
-		std::cout << "Failed to compile vertex shader!\n" << message << std::endl;
-	}
+	// Texture loading
+	Texture heightmap = RM::loadTexture("C:/dev/GitHub/image-to-terrain/image-to-terrain/res/images/noise.png");
 
-	unsigned int fragment = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment, 1, &fragmentSource, 0);
-	glCompileShader(fragment);
-
-	glGetShaderiv(vertex, GL_COMPILE_STATUS, &compiled);	// Check compilation status
-	if(compiled != GL_TRUE)
-	{
-		int log_length = 0;
-		char message[512];
-		glGetShaderInfoLog(vertex, 512, &log_length, message);
-		std::cout << "Failed to compile vertex shader!\n" << message << std::endl;
-	}
-
-	// Program linking
-	unsigned int program = glCreateProgram();
-	glAttachShader(program, vertex);
-	glAttachShader(program, fragment);
-	glLinkProgram(program);
-
-	// Clean up
-	glDetachShader(program, vertex);
-	glDetachShader(program, fragment);
-	glDeleteShader(vertex);
-	glDeleteShader(fragment);
-
-	// Texture creation
-	int width, height, channels;
-	stbi_set_flip_vertically_on_load(true);
-	unsigned char *data = stbi_load("C:/dev/GitHub/image-to-terrain/image-to-terrain/res/images/noise.png", &width, &height, &channels, 4);
-	if(data == nullptr)
-	{
-		std::cout << "Failed to load image!" << std::endl;
-	}
-
-	unsigned int texture;
-	glGenTextures(1, &texture);
-	
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-
-	stbi_image_free(data);
-
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	int tWidth = heightmap.width;
+	int tHeight = heightmap.height;
 
 	std::vector<float> vPos;
-	vPos.reserve(width * height * 12);
+	vPos.reserve(tWidth * tHeight * 12);
 
 	std::vector<unsigned int> indices;
-	indices.reserve(width * height * 6);
+	indices.reserve(tWidth * tHeight * 6);
 
 	std::vector<float> texPos;
-	texPos.reserve(width * height * 8);
+	texPos.reserve(tWidth * tHeight * 8);
 
 	unsigned int index = 0;
-
-	for(int i = 0; i < width; i++)
-	for(int j = 0; j < height; j++)
+	float scale = 10.0f;
+	for(int i = 0; i < tWidth; i++)
+	for(int j = 0; j < tHeight; j++)
 	{
 		std::vector<float> currentVPos
 		{
@@ -164,13 +87,12 @@ int main() {
 		indices.insert(indices.end(), currentIndices.begin(), currentIndices.end());
 		index += 4;
 
-		// TODO: possible wrong order of width and height
 		std::vector<float> currentTexCoordinates
 		{
-			(float)i / width, (float)j / height,
-			(float)(i+1) / width, (float)j / height,
-			(float)(i+1) / width, (float)(j+1) / height,
-			(float)i / width, (float)(j+1) / height
+			(float)i / tWidth, (float)j / tHeight,
+			(float)(i+1) / tWidth, (float)j / tHeight,
+			(float)(i+1) / tWidth, (float)(j+1) / tHeight,
+			(float)i / tWidth, (float)(j+1) / tHeight
 		};
 
 		texPos.insert(texPos.end(), currentTexCoordinates.begin(), currentTexCoordinates.end());
@@ -208,33 +130,44 @@ int main() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-	glUseProgram(program);
+	basicShader.use();
+	basicShader.setInt(glGetUniformLocation(basicShader.program, "tex"), 0);
 
-	int loc = glGetUniformLocation(program, "tex");
-	glUniform1i(loc, 0);
+	int modelLoc = glGetUniformLocation(basicShader.program, "model");
+	int projectionLoc = glGetUniformLocation(basicShader.program, "projection");
+	int viewLoc = glGetUniformLocation(basicShader.program, "view");
+	int scaleLoc = glGetUniformLocation(basicShader.program, "scale");
 
-	int modelLoc = glGetUniformLocation(program, "model");
-	int projectionLoc = glGetUniformLocation(program, "projection");
-	int viewLoc = glGetUniformLocation(program, "view");
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 1500.0f);
+	basicShader.setMat4(projectionLoc, projection);
 
 	int vertexCount = (int)indices.size();
 
-	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 1500.0f);
-	glUniformMatrix4fv(projectionLoc, 1, false, glm::value_ptr(projection));
-	
 	while(!glfwWindowShouldClose(window)) {
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		// Calculating delta time
+		double currentTime = glfwGetTime();
+		deltaTime = currentTime - lastFrame;
+		lastFrame = currentTime;
+
+		char title[10];
+		title[9] = '\0'; 
+		snprintf(title, 9, "%f", 1.0f / deltaTime);
+
+		glfwSetWindowTitle(window, title);
+
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		processInput(window, scale);
+		basicShader.setFloat(scaleLoc, scale);
+
 		glm::mat4 model(1.0f);
-		model = glm::translate(model, { -(float)width / 2.0f, -(float)width / 6.0f, -(float)width * 1.7f });
-		glUniformMatrix4fv(modelLoc, 1, false, glm::value_ptr(model));
+		model = glm::translate(model, { -(float)tWidth / 2.0f, -(float)tWidth / 6.0f, -(float)tWidth * 1.7f });
+		basicShader.setMat4(modelLoc, model);
 
 		glm::mat4 view(1.0f);
-		glUniformMatrix4fv(viewLoc, 1, false, glm::value_ptr(view));
+		basicShader.setMat4(viewLoc, view);
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture);
+		heightmap.bind();
 
 		glBindVertexArray(VAO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -244,8 +177,7 @@ int main() {
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, 0);
+		heightmap.unbind();
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -253,4 +185,12 @@ int main() {
 
 	glfwTerminate();
 	return 0;
+}
+
+void processInput(GLFWwindow* window, float& scale)
+{
+	if(glfwGetKey(window, GLFW_KEY_KP_ADD) == GLFW_PRESS)
+		scale++;
+	if(glfwGetKey(window, GLFW_KEY_KP_SUBTRACT) == GLFW_PRESS)
+		scale--;
 }
